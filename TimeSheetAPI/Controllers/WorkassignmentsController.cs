aab8001a925be1a2eaf2e5d157e5c663d9d3186f
Valter Backström <dylandante@gmail.com>
2024-10-04 13:52:53 +0300

    added httpget to workassignmentscontroller and new class Operation

--- TimeSheetAPI/Controllers/WorkassignmentsController.cs
+++ TimeSheetAPI/Controllers/WorkassignmentsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TimeSheetAPI.Models;
+using TimeSheetApp.Models;
 
 namespace TimeSheetAPI.Controllers
 {
@@ -23,5 +24,37 @@ namespace TimeSheetAPI.Controllers
             var workAssignments = db.WorkAssignments.Where(w => w.Active == true);
             return Ok(workAssignments);
         }
+
+        // GET: api/Workassignments/5 START
+        [HttpGet("start")]
+        public bool Start([FromBody]Operation op)
+        {
+            //Muokataan tietokannassa olevaa WA riviä
+            WorkAssignment? wa = db.WorkAssignments.Find(op.WorkAssignmentID);
+
+            wa.InProgress = true;
+            wa.WorkStartedAt = DateTime.Now.AddHours(1);
+            db.SaveChanges();
+
+            //Luodaan uusi Timesheet rivi tietokantaan
+            Timesheet newEntry = new Timesheet()
+            {
+                IdWorkAssignment = op.WorkAssignmentID,
+                StartTime = DateTime.Now.AddHours(1),
+                Active = true,
+                IdEmployee = op.EmployeeID,
+                IdCustomer = wa.IdCustomer
+                CreatedAt = DateTime.Now.AddHours(1),
+                Comments = op.Comment
+            };
+
+            db.Timesheets.Add(newEntry);
+
+            db.SaveChanges();
+
+            return (true);
+        }
+
+
     }
 }
---
+++ TimeSheetAPI/Models/Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSheetApp.Models
+{
+    public class Operation
+    {
+        public int EmployeeID { get; set; }
+        public int WorkAssignmentID { get; set; }
+        public string OperationType { get; set; }
+        public string Comment { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+    }
+}
